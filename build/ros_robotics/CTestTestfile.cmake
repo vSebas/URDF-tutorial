c06bafe77c1ddf4caffa38c2a764ab9584337213
vSebas <sebas.martp@gmail.com>
2020-06-20 23:37:43 -0500

    URDF tutorial

---
+++ build/ros_robotics/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sebastian/ROS/ROS_tutorials/URDF/src/ros_robotics
+# Build directory: /home/sebastian/ROS/ROS_tutorials/URDF/build/ros_robotics
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
